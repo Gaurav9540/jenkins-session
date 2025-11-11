12dda740df0a77bcbbb7901adf17519275e1fd08
gaurav <gauravpawar9540@gmail.com>
2025-11-11 16:14:37 +0530

    added docker stage

--- demo.groovy
+++ demo.groovy
@@ -15,15 +15,50 @@ pipeline {
             }
         }
 
-        stage('Test'){
+        // stage('Test'){
+        //     steps {
+        //         echo "testing successfully!"
+        //     }
+        // }
+
+
+        // Docker Image Build Stage
+        stage('Docker Build') {
             steps {
-                echo "testing successfully!"
+                script {
+                    def shortSha = sh(returnStdout: true, script: "git rev-parse --short HEAD").trim()
+                    env.IMAGE_TAG = "${env.BUILD_NUMBER}-${shortSha}"
+                }
+                sh "docker build -t ${IMAGE}:${IMAGE_TAG} ."
+                sh "docker images | grep ${APP_NAME}"
+                echo "Docker image built successfully!"
             }
         }
 
-        stage('Deploy on Docker'){
+        // Docker Image Push to Dockerhub Stage        
+        stage('Docker Push') {
             steps {
-                echo "deploy successfully!"
+                withCredentials([usernamePassword(credentialsId: 'dockerhub-creds', usernameVariable: 'USER', passwordVariable: 'PASS')]) {
+                    sh """
+                      echo "$PASS" | docker login -u "$USER" --password-stdin
+                      docker tag ${IMAGE}:${IMAGE_TAG} ${IMAGE}:latest
+                      docker push ${IMAGE}:${IMAGE_TAG}
+                      docker push ${IMAGE}:latest
+                      docker logout
+                    """
+                }
+                echo "Image pushed successfully to Docker Hub!"
+            }
+        }
+
+        // Run Container
+        stage('Deploy with Docker') {
+           steps {
+               sh """
+                  docker rm -f ${APP_NAME} || true
+                  docker run -d --name ${APP_NAME} -p 8083:8080 ${IMAGE}:latest
+                """
+                echo "Container deployed successfully and running on port 8083!"
             }
         }
     }
