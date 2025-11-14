083dc57b1b6234cffbf89901abac60c2b49c43ef
gaurav <gauravpawar9540@gmail.com>
2025-11-14 15:40:56 +0530

    added testing stage

--- demo.groovy
+++ demo.groovy
@@ -28,6 +28,24 @@ pipeline {
         //     }
         // }
 
+        // Testing Stage
+        stage('Test') {
+            steps {
+                withSonarQubeEnv(installationName: 'sonar-server', credentialsId: 'sonar-token') {
+                  sh 'mvn clean verify sonar:sonar -Dsonar.projectKey=myproject'
+                }
+                echo "testing successfully!"
+            }
+        }
+
+        // QualityGate Check Test
+        stage('QualityGate') {
+            steps {
+                waitForQualityGate abortPipeline: false, credentialsId: 'sonar-secret-key'
+                echo "qulity gate check successfully!"
+            }
+        }
+
 
         // Docker Image Build Stage
         stage('Docker Build') {
